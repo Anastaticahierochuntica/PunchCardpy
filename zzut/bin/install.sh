5244a8748e63ee69ed3e97f35bdb08e03aa410c0
wzmmdsnn@163.com <wzmmdsnn@163.com>
2020-11-28 10:57:51 +0800

    初步编写依赖安装脚本

---
+++ zzut/bin/install.sh
@@ -0,0 +1,28 @@
+# !/bin/bash
+# 安装火狐浏览器
+sudo yum install firefox -y
+
+# 安装python3环境
+sudo yum install python3 -y
+
+# 安装python3依赖库
+sudo pip3 install pyvirtualdisplay
+sudo pip3 install urllib
+sudo pip3 install fake_useragent
+sudo pip3 install selenium
+
+# 安装运行依赖
+sudo yum install gtk3 gtk3-devel -y
+sudo yum install Xvfb libXfont xorg-x11-fonts* -y
+sudo yum install xdpyinfo -y
+
+# 下载firefox驱动：
+wget https://github.com/mozilla/geckodriver/releases/download/v0.28.0/geckodriver-v0.28.0-linux64.tar.gz
+
+# 解压缩：
+tar -zxvf ./geckodriver-v0.28.0-linux64.tar.gz
+
+# 软链接：
+ln -s $(pwd)/geckodriver /usr/bin/geckodriver
+
+rm -f ./geckodriver-v0.28.0-linux64.tar.gz
---
+++ zzut/bin/taskstart.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+ps -aux | grep "zzut_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
+
+if [ ! -d ../log ]; then
+    mkdir log
+fi
+
+nohup python3 ./zzut_punchcard_start.py >>../log/runtime.log 2>>../log/runtime.log &
