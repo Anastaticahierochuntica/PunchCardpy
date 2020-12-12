7c53b20ad0df79a976553d842fd4651f2238c562
wzmmdsnn@163.com <wzmmdsnn@163.com>
2020-12-12 17:39:51 +0800

    随机分配

--- zzut/bin/shutdown.sh
+++ zzut/bin/shutdown.sh
@@ -1,5 +1,29 @@
 # !/bin/bash
 
-ps -aux | grep "zzut_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
+echo "1. 关闭快速打卡模式"
+echo "2. 关闭随机分布打卡模式"
+echo "3. 关闭全变量打卡模式"
+echo "4. 全部关闭"
 
-echo "服务关闭"
+read -p "选择需要关闭的模式：" pattern
+
+case $pattern in
+1)
+    ps -aux | grep "zzut_speed_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
+    echo "快速打卡模式已关闭"
+    ;;
+2)
+    ps -aux | grep "zzut_rd_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
+    echo "随机分布打卡模式已关闭"
+    ;;
+3)
+    ps -aux | grep "zzut_all_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
+    ;;
+4)
+    ps -aux | grep "zzut_speed_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
+    ps -aux | grep "zzut_rd_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
+    ps -aux | grep "zzut_all_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
+    echo "所有模式已全部关闭"
+    ;;
+
+esac
--- zzut/bin/taskstart.sh
+++ zzut/bin/taskstart.sh
@@ -1,11 +1,38 @@
 # !/bin/bash
 
-ps -aux | grep "zzut_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
-
 if [ ! -d ../log ]; then
     mkdir ../log
 fi
 
-nohup python3 $(pwd)/zzut_punchcard_start.py >>../log/runtime.log 2>>../log/runtime.log &
+echo "1. 开启快速打卡模式"
+echo "2. 开启随机分布打卡模式"
+echo "3. 开启全变量打卡模式"
+echo "4. 全部开启"
 
-echo "服务开启"
+read -p "选择需要的模式：" pattern
+
+case $pattern in
+1)
+    ps -aux | grep "zzut_speed_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
+    nohup python3 $(pwd)/zzut_speed_punchcard_start.py >>../log/runtime.log 2>>../log/runtime.log &
+    echo "快速打卡模式已开启"
+    ;;
+2)
+    ps -aux | grep "zzut_rd_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
+    nohup python3 $(pwd)/zzut_rd_punchcard_start.py >>../log/runtime.log 2>>../log/runtime.log &
+    echo "随机分布打卡模式已开启"
+    ;;
+3)
+    ps -aux | grep "zzut_all_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
+    nohup python3 $(pwd)/zzut_all_punchcard_start.py >>../log/runtime.log 2>>../log/runtime.log &
+    ;;
+4)
+    ps -aux | grep "zzut_speed_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
+    ps -aux | grep "zzut_rd_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
+    ps -aux | grep "zzut_all_punchcard_start" | grep -v grep | awk '{print "kill -9 "$2}' | sh
+    nohup python3 $(pwd)/zzut_speed_punchcard_start.py >>../log/runtime.log 2>>../log/runtime.log &
+    nohup python3 $(pwd)/zzut_rd_punchcard_start.py >>../log/runtime.log 2>>../log/runtime.log &
+    nohup python3 $(pwd)/zzut_all_punchcard_start.py >>../log/runtime.log 2>>../log/runtime.log &
+    echo "所有模式已全部开启"
+    ;;
+esac
